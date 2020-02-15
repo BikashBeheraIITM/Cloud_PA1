a93e3857bf18a5c1302d39110c1ac7e94a748cf1
Bikash Kumar Behera <psychopanda@KaybeePanda.Bamboo>
2020-02-15 14:44:11 +0530

    lostcount 5

--- PA1/client.py
+++ PA1/client.py
@@ -1,20 +1,17 @@
 import asyncio
 from aiohttp import ClientSession
-rate = 1000
-async def fetch(url):
-    async with ClientSession() as s, s.get(url) as res:
+rate = 10
+async def send(url,data):
+    async with ClientSession() as s, s.post(url,data = data) as res:
         ret = await res.read()
-        return ret
+        resp = ret.json()
+        print(resp)
 
 async def print_when_done(tasks):
-    i=0
     for res in asyncio.as_completed(tasks):
         print(await res)
-        print("------------------------------------------------------------------------------------------------------------")
-        print(i)
-        i+=1
 
-mil_req = [fetch("http://cloudpa1.us-east-1.elasticbeanstalk.com/") for i in range(rate)]
+mil_req = [send("localhost:8080/",i) for i in range(rate)]
 
 asyncio.run(print_when_done(mil_req))
 
