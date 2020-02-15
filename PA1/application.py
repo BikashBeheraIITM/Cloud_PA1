ff68b9d463a4c4588770f975281ae8d045c60c49
Bikash Kumar Behera <psychopanda@KaybeePanda.Bamboo>
2020-02-15 15:45:25 +0530

    lostcount10

--- PA1/application.py
+++ PA1/application.py
@@ -1,14 +1,20 @@
 from aiohttp import web
 import json
+def fib (n,store={0:0,1:1}):
+    if n not in store:
+        store[n] = fib(n-1,store) + fib(n-2,store)
+    return store[n]
 
 async def handle(request):
     response_obj = {'status':'success'}
     return web.Response(text=json.dumps(response_obj),status=200)
 
-async def new_user(request):
+async def post_handle(request):
     try:
-        user = request.query['name']
-        response_obj = {'status':'success','message':str(user)+' created successfully'}
+        val = request.query['n']
+        num = int(val)
+        f = fib(num)
+        response_obj = {'status':'success','message':'The '+str(num)+'th fibonacci number is: '+str(f)}
         return web.Response(text=json.dumps(response_obj),status=200)
     except Exception as e:
         response_obj = {'status':'failed','message':str(e)}
@@ -16,5 +22,5 @@ async def new_user(request):
 
 application = web.Application()
 application.router.add_get('/',handle)
-application.router.add_post('/user',new_user)
+application.router.add_post('/',post_handle)
 web.run_app(application)
--- PA1/client.py
+++ PA1/client.py
@@ -1,17 +1,17 @@
 import asyncio
 from aiohttp import ClientSession
-rate = 10
-async def send(url,data):
-    async with ClientSession() as s, s.post(url,data = data) as res:
+rate = 10000
+async def send(url,i):
+    data = '?n='+str(i)
+    async with ClientSession() as s, s.post(url+data) as res:
         ret = await res.read()
-        resp = ret.json()
-        print(resp)
+        return ret
 
 async def print_when_done(tasks):
     for res in asyncio.as_completed(tasks):
-        print(await res)
+        await res
 
-mil_req = [send("localhost:8080/",i) for i in range(rate)]
+mil_req = [send("http://localhost:8080/",i) for i in range(rate)]
 
 asyncio.run(print_when_done(mil_req))
 
